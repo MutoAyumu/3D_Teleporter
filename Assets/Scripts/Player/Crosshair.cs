3e5002daee971297c00c852e6c56bad170252ea8
MutoAyumu <mutoayum0610@gmail.com>
2021-12-10 00:17:50 +0900

    Crosshairスクリプトを作った

---
+++ Assets/Scripts/Player/Crosshair.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    [SerializeField] Image[] _portalImage = default;
+
+    private void Start()
+    {
+        _portalImage[0].gameObject.SetActive(false);
+        _portalImage[1].gameObject.SetActive(false);
+    }
+    public void SetPortal(int portalID, bool isSet)
+    {
+        if(portalID == 0)
+        {
+            _portalImage[0].gameObject.SetActive(isSet);
+        }
+        else
+        {
+            _portalImage[1].gameObject.SetActive(isSet);
+        }
+    }
+}
