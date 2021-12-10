55a7b0b732f1c77c170eed05d63f3f3e48d2d0a7
MutoAyumu <mutoayum0610@gmail.com>
2021-12-10 18:44:56 +0900

    SetUpPortalスクリプトを作った

--- Assets/Scripts/Player/Crosshair.cs
+++ Assets/Scripts/Player/Crosshair.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 
 public class Crosshair : MonoBehaviour
 {
+    [SerializeField] PortalScript[] _portals = default;
     [SerializeField] Image[] _portalImage = default;
 
     private void Start()
     {
+        _portalImage[0].color = _portals[0].PortalColor;
+        _portalImage[1].color = _portals[1].PortalColor;
+
         _portalImage[0].gameObject.SetActive(false);
         _portalImage[1].gameObject.SetActive(false);
     }
---
+++ Assets/Scripts/Player/SetUpPortal.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUpPortal : MonoBehaviour
+{
+    [SerializeField] PortalScript[] _portals = default;
+    [SerializeField] LayerMask _wallLayer = default;
+    [SerializeField] LayerMask _portalLayer = default;
+    [SerializeField] Crosshair _crosshair = default;
+    [SerializeField] float _rayDistance = 50f;
+    
+    PlayerController _player = default;
+    private void Awake()
+    {
+        _player = GetComponent<PlayerController>();
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            FirePortal(0, this.transform.position, this.transform.forward, _rayDistance);
+        }
+        else if(Input.GetButtonDown("Fire2"))
+        {
+            FirePortal(1, this.transform.position, this.transform.forward, _rayDistance);
+        }
+    }
+    void FirePortal(int portalID, Vector3 pos, Vector3 dir, float distance)
+    {
+        RaycastHit hit;
+        Physics.Raycast(pos, dir, out hit, distance, _wallLayer);
+
+        if(hit.collider != null)
+        {
+            if(hit.collider.gameObject.layer == _portalLayer)
+            {
+                //ここ必要かわからんから後回しにするわ
+            }
+
+            //カメラの向きをポータルの方向と合わせる
+            var cameraRotation = _player.TargetRotation;
+            var portalRight = cameraRotation * Vector3.right;
+
+            if (Mathf.Abs(portalRight.x) >= Mathf.Abs(portalRight.z))
+            {
+                portalRight = (portalRight.x >= 0) ? Vector3.right : -Vector3.right;
+            }
+            else
+            {
+                portalRight = (portalRight.z >= 0) ? Vector3.forward : -Vector3.forward;
+            }
+
+            var portalForward = -hit.normal;
+            var portalUp = -Vector3.Cross(portalRight, portalForward);
+
+            var portalRotation = Quaternion.LookRotation(portalForward, portalUp);
+
+            bool wasPlaced = _portals[portalID].PlacePortal(hit.collider, hit.point, portalRotation);
+
+            if(wasPlaced)
+            {
+                _crosshair.SetPortal(portalID, true);//クロスヘアのImageを変更する
+            }
+        }
+    }
+}
