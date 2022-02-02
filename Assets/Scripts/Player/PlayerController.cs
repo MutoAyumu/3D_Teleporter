34184303185bd07965eb4961c498709a7faa567e
MutoAyumu <mutoayum0610@gmail.com>
2022-02-03 02:11:38 +0900

    ダメージを受けた時に画面が変化するようにした

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,14 @@
 ﻿using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
 
 /// <summary>プレイヤーにつける</summary>
 public class PlayerController : PortalableObject
 {
     [Space(10), Header("CameraMove")]
-    [SerializeField]float _cameraSpeed = 3f;
+    [SerializeField] float _cameraSpeed = 3f;
     [SerializeField, Tooltip("XがMin　YがMax")] Vector2 _cameraValue = Vector2.zero;
     [SerializeField] float _slerpSpeed = 15f;
     [SerializeField] Transform _eye = default;
@@ -18,17 +19,26 @@ public class PlayerController : PortalableObject
     [SerializeField] float _jumpPower = 15f;
     [SerializeField] string _settingButtonName = "Jump";
     [SerializeField] float _hitPoint = 100;
+    [SerializeField, Tooltip("ダメージを受けてから回復するまでの時間")] float _recoveryTime = 7f;
+    [SerializeField, Tooltip("全回復するまでの時間")] float _recoverPoint = 5f;
     [Space(10), Header("IsGround")]
     [SerializeField] Transform _footPos = default;
     [SerializeField] Vector3 _rayDistance = Vector3.zero;
     [SerializeField] LayerMask _groundLayer = default;
+    [SerializeField] PostProcessVolume _playerVolume = default;
+    Vignette _vignette;
+    ChromaticAberration _chromatic;
 
     Quaternion _targetRotation;
     Vector3 _dir;
     bool isGround;
     bool isPause;
+    bool isDamage;
+    bool isRecover;
     Animator _anim;
     float _animSpeed;
+    float _currentHP;
+    float _timer;
 
     public Quaternion TargetRotation { get => _targetRotation; set => _targetRotation = value; }
     protected override void Awake()
@@ -38,6 +48,10 @@ public class PlayerController : PortalableObject
         TargetRotation = this.transform.rotation;
         _anim = _body.GetComponent<Animator>();
         _animSpeed = _anim.speed;
+        _currentHP = _hitPoint;
+        var profile = _playerVolume.profile;
+        _vignette = profile.GetSetting<Vignette>();
+        _chromatic = profile.GetSetting<ChromaticAberration>();
     }
 
     private void Update()
@@ -49,6 +63,7 @@ public class PlayerController : PortalableObject
             UpdateMove();
             InputJump();
             IsGround();
+            Recovery();
         }
     }
     /// <summary>カメラの回転を制御する</summary>
@@ -57,7 +72,7 @@ public class PlayerController : PortalableObject
         var rotation = new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
         var targetEuler = TargetRotation.eulerAngles + (Vector3)rotation * _cameraSpeed;
 
-        if(targetEuler.x > 180.0f)
+        if (targetEuler.x > 180.0f)
         {
             targetEuler.x -= 360.0f;
         }
@@ -86,7 +101,7 @@ public class PlayerController : PortalableObject
             _rb.velocity = new Vector3(0, _rb.velocity.y, 0);
             _gunModel.SetBool("Walk", false);
 
-            if(isGround)
+            if (isGround)
             {
                 _anim.SetBool("Walk", false);
             }
@@ -99,8 +114,8 @@ public class PlayerController : PortalableObject
             Vector3 move = _dir.normalized * _moveSpeed;　//移動
             move.y = _rb.velocity.y;
             _rb.velocity = move;
-            
-            if(isGround)
+
+            if (isGround)
             {
                 _anim.SetBool("Walk", true);
                 _gunModel.SetBool("Walk", true);
@@ -109,9 +124,9 @@ public class PlayerController : PortalableObject
     }
     void InputJump()
     {
-        if(isGround)
+        if (isGround)
         {
-            if(Input.GetButtonDown(_settingButtonName))
+            if (Input.GetButtonDown(_settingButtonName))
             {
                 _rb.AddForce(this.transform.up * _jumpPower, ForceMode.Impulse);
             }
@@ -133,16 +148,66 @@ public class PlayerController : PortalableObject
             _anim.SetBool("IsGround", false);
         }
     }
-    public void Damage(float damage)
+    public void Damage(int damage)
     {
-        if(_hitPoint > 0)//体力が0以上の時は計算する
+        if (_currentHP > 0)//体力が0以上の時は計算する
         {
-            _hitPoint -= damage;
-            Debug.Log(_hitPoint);
+            _currentHP -= damage;
+            isDamage = true;
+            _timer = 0;
+
+            if (_vignette.intensity.value <= 0.5f)
+            {
+                _vignette.intensity.value += 0.5f / _hitPoint;
+            }
+            if (_chromatic.intensity.value <= 1)
+            {
+                _chromatic.intensity.value += 1 / _hitPoint;
+            }
+            Debug.Log("HP" + _currentHP + "Vignette" + _vignette.intensity.value + "Chromatic" + _chromatic.intensity.value);
         }
         else
         {
             Debug.Log("Dead");
+        }
+    }
+    void Recovery()
+    {
+        if (isDamage)
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer >= _recoveryTime)//タイマーの方が大きくなったら回復を始める
+            {
+                _timer = 0;
+                isDamage = false;
+                isRecover = true;
+                Debug.Log("回復を始めます");
+            }
+        }
+        else if (!isDamage && isRecover)
+        {
+            _currentHP += _hitPoint / _recoverPoint * Time.deltaTime;
+
+            if (_vignette.intensity.value > 0)
+            {
+                _vignette.intensity.value -= 0.5f / _recoverPoint * Time.deltaTime;
+            }
+            if (_chromatic.intensity.value > 0)
+            {
+                _chromatic.intensity.value -= 1 / _recoverPoint * Time.deltaTime;
+            }
+
+            Debug.Log("HP" + _currentHP + " Vignette" + _vignette.intensity.value + " Chromatic" + _chromatic.intensity.value);
+
+            if (_currentHP > _hitPoint)
+            {
+                isRecover = false;
+                _currentHP = _hitPoint;
+                _chromatic.intensity.value = 0;
+                _vignette.intensity.value = 0;
+                Debug.Log("全回復しました");
+            }
         }
     }
     public override void Warp()
