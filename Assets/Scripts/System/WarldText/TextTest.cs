a2c8442fa215e8c21882648313e72786b697fd4d
MutoAyumu <mutoayum0610@gmail.com>
2022-02-09 16:06:58 +0900

    近づくと出てくるテキストを作った

---
+++ Assets/Scripts/System/WarldText/TextTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTest : MonoBehaviour
+{
+    private void Start()
+    {
+        var mtf = this.transform.localScale;
+        transform.localScale = new Vector3(-mtf.x, mtf.y, mtf.z);
+    }
+    private void LateUpdate()
+    {
+        this.transform.LookAt(Camera.main.transform);
+    }
+}
